577ae64504f8e45732085abab15691e4c34815f3
Hari Govardhanam <harigov@gmail.com>
2017-07-16 15:30:46 -0700

    Build and deployment automation for extensions

---
+++ automate/build_extension.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python thirdparty/kango/kango.py build extension
---
+++ automate/release_extension.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+export GITHUB_TOKEN='INSERT_TOKEN_HERE'
+export GITHUB_RELEASE_TOOL=~/bin/github-release
+export USER_NAME='harigov'
+export REPOSITORY_NAME='newsalyzer'
+export EXTENSION_VERSION='0.1.0'
+export RELEASE_VERSION="v$EXTENSION_VERSION"
+export RELEASE_NAME='Newsalyzer Browser Extension'
+export RELEASE_DESCRIPTION='Initial version of the extension'
+
+EXTENSION_FILE_NAME=$REPOSITORY_NAME
+EXTENSION_FILE_NAME+='_'
+EXTENSION_FILE_NAME+=$EXTENSION_VERSION
+
+echo 'Create a new release tag in github'
+$GITHUB_RELEASE_TOOL release --user $USER_NAME \
+		--repo $REPOSITORY_NAME \
+		--tag $RELEASE_VERSION \
+		--name "$RELEASE_NAME" \
+		--description "$RELEASE_DESCRIPTION" \
+		--pre-release
+
+echo 'Upload chrome version of the extension'
+$GITHUB_RELEASE_TOOL upload --user $USER_NAME \
+		--repo $REPOSITORY_NAME \
+		--tag $RELEASE_VERSION \
+		--name "$REPOSITORY_NAME-chrome-$EXTENSION_VERSION.crx" \
+		--file "extension/output/$EXTENSION_FILE_NAME.crx"
+
+echo 'Upload firefox version of the extension'
+$GITHUB_RELEASE_TOOL upload --user $USER_NAME \
+		--repo $REPOSITORY_NAME \
+		--tag $RELEASE_VERSION \
+		--name "$REPOSITORY_NAME-firefox-$EXTENSION_VERSION.xpi" \
+		--file "extension/output/$EXTENSION_FILE_NAME.xpi"
