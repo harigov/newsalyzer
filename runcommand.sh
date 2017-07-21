f9158d6a9d4d11aa69b15ece6903658835b1b97c
erasmus <dfcf93@hotmail.com>
2017-07-21 08:57:02 -0700

    removed articles and added script

---
+++ runcommand.sh
@@ -0,0 +1,8 @@
+python crawler.py https://www.nytimes.com/section/world 10000 /home/erasmus/Data/nytimes/world
+python crawler.py https://www.nytimes.com/section/us 10000 /home/erasmus/Data/nytimes/us
+python crawler.py https://www.nytimes.com/section/politics 10000 /home/erasmus/Data/nytimes/politics
+python crawler.py https://www.nytimes.com/pages/opinion 10000 /home/erasmus/Data/nytimes/opinion
+python crawler.py http://www.nytimes.com/pages/business 10000 /home/erasmus/Data/nytimes/business
+python crawler.py https://www.nytimes.com/section/technology 10000 /home/erasmus/Data/nytimes/technology
+python crawler.py https://www.nytimes.com/section/science 10000 /home/erasmus/Data/nytimes/science
+python crawler.py https://www.nytimes.com/section/health 10000 /home/erasmus/Data/nytimes/health
