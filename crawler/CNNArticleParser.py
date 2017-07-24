17c292c3ff6c001b533c04ce027ef2122c48bb37
erasmus <dfcf93@hotmail.com>
2017-07-24 16:31:41 -0700

    Cnn Crawler

---
+++ crawler/CNNArticleParser.py
@@ -0,0 +1,37 @@
+import urlparse
+import os
+from bs4 import BeautifulSoup
+import re
+from crawler import ArticleParse
+class CNNArticleParser(object):
+    def parse(self, url, article_text):
+        ArticleParser.parse(self,url,article_text)
+
+    def _get_title(self):
+        head = self._soup.find('pg-headline')
+        if head != None:
+            title = head.find('pg-headline')
+            if title != None:
+                return title.getText()
+        return ''
+    def _get_date(self):
+        date = self._soup.find('li', {'class' : 'update-time'})
+        if(date != None):
+            return date.getText().encode('utf-8')
+        else:
+            date = self._soup.find('meta', {'name' : 'DISPLAYDATE'})
+            if date != None:
+                date = date.get('content')
+                if date != None:
+                    return date.encode('utf-8')
+        return ''
+    def _get_link_elements(self):
+        return self._soup.findAll('div', {'class' : 'story'}) + self._soup.findAll('article', {'class' : 'story'})
+    def _get_article_text(self):
+        content = ''
+        zn-body__paragraph
+        for story_element in self._soup.findAll('p', {'class' : 'zn-body__paragraph'}):
+            if story_element != None:
+                # Remove newlines
+                content += re.sub(r"\n+", " ", story_element.getText())
+        return content
